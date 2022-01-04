7c92f222da7680093c72985eb886c9304715d0ce
unknown <mntanveer9196@gmail.com>
2022-01-04 11:17:44 +0600

    products page

--- src/pages/Products.js
+++ src/pages/Products.js
@@ -7,7 +7,7 @@ const Products = () => {
     <>
       {/* filter */}
 
-      <div className="2xl:container 2xl:mx-auto">
+      <div className="2xl:container 2xl:mx-auto sticky top-0 z-50 bg-white border-b border-slate-200">
         <div className="md:py-12 lg:px-20 md:px-6 py-9 px-4">
           <p className="text-sm leading-3 text-gray-400 font-normal mb-2">
             Groceries - All
@@ -57,6 +57,31 @@ const Products = () => {
       <div className="bg-white">
         <div className="max-w-2xl mx-auto px-4 sm:px-6 lg:max-w-7xl lg:px-8">
           <div className="mt-6 grid grid-cols-1 gap-y-10 gap-x-6 sm:grid-cols-2 lg:grid-cols-4 xl:gap-x-8">
+            {/* single product */}
+            <div className="group relative">
+              <div className="w-full min-h-80 bg-gray-200 aspect-w-1 aspect-h-1 rounded-md overflow-hidden group-hover:opacity-75 lg:h-80 lg:aspect-none">
+                <img
+                  src="https://tailwindui.com/img/ecommerce-images/product-page-01-related-product-01.jpg"
+                  alt="Front of men&#039;s Basic Tee in black."
+                  className="w-full h-full object-center object-cover lg:w-full lg:h-full"
+                />
+              </div>
+              <div className="mt-4 flex justify-between">
+                <div>
+                  <h3 className="text-sm text-gray-700">
+                    <a href="#">
+                      <span
+                        aria-hidden="true"
+                        className="absolute inset-0"
+                      ></span>
+                      Basic Tee
+                    </a>
+                  </h3>
+                  <p className="mt-1 text-sm text-gray-500">Black</p>
+                </div>
+                <p className="text-sm font-medium text-gray-900">$35</p>
+              </div>
+            </div>
             {/* single product */}
             <div className="group relative">
               <div className="w-full min-h-80 bg-gray-200 aspect-w-1 aspect-h-1 rounded-md overflow-hidden group-hover:opacity-75 lg:h-80 lg:aspect-none">
