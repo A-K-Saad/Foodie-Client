4a0e5c1b57bdfc0714b05a064b88e5650870ec89
Afjalul Karim Saad <85860224+A-K-Saad@users.noreply.github.com>
2024-06-29 17:35:51 +0600

    Update firebase.config.js

--- src/Firebase/firebase.config.js
+++ src/Firebase/firebase.config.js
@@ -1,10 +1,10 @@
 const firebaseConfig = {
-  apiKey: "AIzaSyAu78V8jpxZEmWhbmlVkA5wlXzLkguF388",
-  authDomain: "foodie-mart.firebaseapp.com",
-  projectId: "foodie-mart",
-  storageBucket: "foodie-mart.appspot.com",
-  messagingSenderId: "1059166441387",
-  appId: "1:1059166441387:web:4bd4895956d38e7b874cee",
-  measurementId: "G-XE6DV51DM8",
+  apiKey: ,
+  authDomain: ,
+  projectId: ,
+  storageBucket: ,
+  messagingSenderId: ,
+  appId: ,
+  measurementId: ,
 };
 export default firebaseConfig;
